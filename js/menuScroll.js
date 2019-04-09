3f46d5b0a0a9faef8c03f1619dcd75331118da80
Matheus Ferreira <44850549+iammatheus@users.noreply.github.com>
2019-04-09 18:51:35 -0300

    Update menuScroll.js

--- js/menuScroll.js
+++ js/menuScroll.js
@@ -13,7 +13,7 @@ $(function(){
     var windowHeight = $(window).height();
     $('.sessao').each(function(){
       var elOffY = $(this).offset().top;
-      if(elOffY+330 < (windowOffY + windowHeight) && elOffY+330+$(this).height() > windowOffY){
+      if(elOffY+300 < (windowOffY + windowHeight) && elOffY+300+$(this).height() > windowOffY){
         $('a').css('border-bottom','0');
         var target = $(this).attr('target');
         $('.'+target).css('border-bottom','1px solid #f13f55');
